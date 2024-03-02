a6b1652912c40a65b170135f77682496a52f8938
Dan Noonan <dnoonan23@yahoo.com>
2024-03-02 15:50:29 -0600

    initial. added some routing.

---
+++ routes/api/category-routes.js
@@ -0,0 +1,75 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const payload = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.json(payload);
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try {
+    const payload = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.json(payload);
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const payload = await Category.create(req.body)
+    res.json(payload);
+  } catch( err ) {
+    res.status(500).json({ error: err.message })
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  try {
+    const updatedResource = await Category.update(
+      req.body, 
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+    res.json(updatedResource)
+  } catch( err ){
+    res.status(500).json({ error: err.message })
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try {
+    await Category.destroy(
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+    res.json({ status: "ok" })
+  } catch( err ){
+    res.status(500).json({ error: err.message })
+  }
+});
+
+module.exports = router;
